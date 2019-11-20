e11e1e2a0e032dcf9fe7eefb20de5c95057e2961
Maxime <pocard.maxime@gmail.com>
2019-11-20 13:44:00 +0100

    Removed mongodb deprecation warning

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://127.0.0.1:27017/languagesAPI';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-mongoose.connect(mongoDB);
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 const connection = mongoose.connection;
